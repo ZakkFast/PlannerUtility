67aa59a5682a88da276b3364f02d05625112acb1
ZakkFast <Zakkfastpro@gmail.com>
2021-07-11 18:00:26 -0700

    worked on populate function

--- Assets/script/main.js
+++ Assets/script/main.js
@@ -1,55 +1,53 @@
-const currentTimeEl = document.querySelector('#currentDay')
-// const date = moment().format('MMM Do YYYY');
+$currentTime = moment().format('dddd, MMMM Do, YYYY')
+$('#currentDay').text($currentTime)
 
-//  
-
-// function populate(){
-//     for(i = 9; i <= 17; i++){
-//         row = $('<div class="row>fuck</div')
-//         $('container').append(row)
-//     }
-// }
 function populate(){
     for(i = 9; i <= 17; i++){
-        $row = $('<div class= "row">[i]</div')
+        $row = $('<div class= "row"></div')
         $hour = $('<div class = "hour col-1"></div>')
-        $input = $('<div class = "col-10"></div>')
+        $input = $('<textarea class = "col-10"></textarea>')
+        $hour.text(moment(i, 'hh').format('LT'))
         $button = $('<div class = "saveBtn col-1">Save</div>')
         $('.container').append($row)
         $($row).append($hour)
         $($row).append($input)
         $($row).append($button)
+
+        
+        $timeNow = moment().format('HH')
+        $timeBlock = i
+        if($timeBlock == $timeNow){
+            $input.addClass('present')
+        } else if ($timeBlock < $timeNow){
+            $input.addClass('past')
+        } else {
+            $input.addClass('future')
+        }
+
+        console.log($timeNow)
+        // $input.each(function(){
+        //     console.log($timeBlock)
+        //     console.log($timeNow)
+            
+            
+        // })
     }
+    
 }
 
 populate()
 
 
 
+
+
 /*
 1.display time - done
-2.presnet user with time blocks
+2.presnet user with time blocks - done
     2.1 input forms
-    2.2 style
+    2.2 style -done
 
-3.color code time blocks to past present future
-4.allow user to edit content in blocks
+3.color code time blocks to past present future - done
+4.allow user to edit content in blocks 
 5.save content to local storage
 */
-
-//for loop comparison for color block
-// let i = 8
-
-// for (i < 17)
-
-// var < var current time
-
-// if(past){
-//     change class
-// } if(present) {
-//     change class
-// } else if {
-//     change class
-// }
-
-//Write a function that add div class row.
