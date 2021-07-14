3bebbc371f9edb3d0458235645c2762310bd7c59
ZakkFast <Zakkfastpro@gmail.com>
2021-07-13 19:19:41 -0700

    added comments to code

--- Assets/script/main.js
+++ Assets/script/main.js
@@ -3,17 +3,19 @@ $('#currentDay').text($currentTime)
 
 function populate(){
     for(i = 8; i <= 17; i++){
+        //Creates UI 
         $row = $(`<div class= "row">`)
         $hour = $(`<div class = "hour time-block col-1"></div>`)
         $hour.text(moment(i, 'hh').format('LT'))
         $input = $(`<textarea class = "col" id='inputText${i}' type='text' name='userInput'></textarea>`)
         $input.text(localStorage.getItem(i))
         $button = $(`<button class = "saveBtn col-1 col-m-2 fas fa-save" data-id='${i}'></button>`)
+        //Appends UI in order
         $('.container').append($row)
         $($row).append($hour)
         $($row).append($input)
         $($row).append($button)
-        
+        //Setting time formate to be am or pm for easier comparision below.
         $timeNow = moment().format('HH')
         $timeBlock = i
         if($timeBlock == $timeNow){
@@ -23,7 +25,7 @@ function populate(){
         } else {
             $input.addClass('future')
         }
-    }
+    }//Adds event listener to save to local storage
     $(".saveBtn").click(function(){
         var id = $(this).data("id")
         var userInput = $(this).siblings().eq(1).val()
